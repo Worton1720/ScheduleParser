f7d84430fd4622c448c9e4e83c16eef6abb86ba6
Worton <124402406+Worton1720@users.noreply.github.com>
2024-07-09 20:05:14 +0300

    Add files via upload

---
+++ parser/ScheduleFetcher.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+
+public class ScheduleFetcher
+{
+    private readonly string _url;
+    private readonly Dictionary<string, string> _headers;
+
+    public ScheduleFetcher(string url, Dictionary<string, string> headers)
+    {
+        _url = url;
+        _headers = headers;
+    }
+
+    public Dictionary<string, List<Dictionary<string, string>>> FetchSchedule(string endpoint, Dictionary<string, string> parameters = null)
+    {
+        try
+        {
+            var fullUrl = _url + endpoint;
+            if (parameters != null)
+            {
+                var queryString = new StringBuilder();
+                foreach (var param in parameters)
+                {
+                    queryString.Append($"{param.Key}={WebUtility.UrlEncode(param.Value)}&");
+                }
+                fullUrl += "?" + queryString.ToString().TrimEnd('&');
+            }
+
+            var request = (HttpWebRequest)WebRequest.Create(fullUrl);
+            foreach (var header in _headers)
+            {
+                request.Headers.Add(header.Key, header.Value);
+            }
+
+            using (var response = (HttpWebResponse)request.GetResponse())
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                var responseText = streamReader.ReadToEnd();
+                return JsonConvert.DeserializeObject<Dictionary<string, List<Dictionary<string, string>>>>(responseText);
+            }
+        }
+        catch (WebException ex)
+        {
+            Console.WriteLine($"Ошибка при выполнении запроса: {ex.Message}");
+            return null;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла непредвиденная ошибка: {ex.Message}");
+            return null;
+        }
+    }
+}
---
+++ parser/SchedulePrinter.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+public static class SchedulePrinter
+{
+    public static void PrintSchedule(Dictionary<string, List<Dictionary<string, string>>> scheduleData)
+    {
+        if (scheduleData != null)
+        {
+            foreach (var dateEntry in scheduleData)
+            {
+                Console.WriteLine($"Дата: {dateEntry.Key}");
+                foreach (var entry in dateEntry.Value)
+                {
+                    Console.WriteLine($"Урок: {entry["lesson"]}");
+                    Console.WriteLine($"Длительность пары: {entry["started_at"]}|{entry["finished_at"]}");
+                    Console.WriteLine($"Преподаватель: {entry["teacher_name"]}");
+                    Console.WriteLine($"Предмет: {entry["subject_name"]}");
+                    Console.WriteLine($"Аудитория: {entry["room_name"]}");
+                    Console.WriteLine();
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Расписание на указанный день отсутствует.");
+        }
+    }
+
+    public static void SaveToJson(Dictionary<string, List<Dictionary<string, string>>> scheduleData, string filename)
+    {
+        var json = JsonConvert.SerializeObject(scheduleData, Formatting.Indented);
+        File.WriteAllText(filename, json);
+    }
+}
